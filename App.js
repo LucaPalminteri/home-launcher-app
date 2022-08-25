0b8721d98a8e1f4edc01c3440e5fb750063aab34
LucaPalminteri <lucapalminteri02@gmail.com>
2022-08-25 18:40:05 -0300

    fix node_modules problems and add style

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
+import { color } from '@rneui/base';
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Swiper from 'react-native-swiper';
+//import { Camera } from 'expo-camera';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
-const phone = <Icon name="phone" size={25} color="#FFF" />;
-const camera = <Icon name="camera" size={20} color="#FFF" />;
+
+const colors = {
+  main: '#fbf8cc',
+  secondary: '#f5ee84',
+  dark: '#03045e'
+}
+
+
+const phone = <Icon name="phone" size={25} color={colors.dark} />;
+const camera = <Icon name="camera" size={20} color={colors.dark} />;
 const search = <Icon name="magnifying-glass" size={30} color="#FFF" />;
 const settings = <Icon name="gear" size={30} color="#FFF" />;
 
@@ -23,70 +33,77 @@ export default function App() {
 
   return (
     <View style={styles.app}>
-      <Swiper style={styles.wrapper} showsButtons={true}>
-      <View style={styles.containerClock}>
-        <Text style={styles.hour}>{hour}</Text>
-        <Text style={styles.date}>{today}</Text>
-      </View>
-      <View style={styles.footer}>
-        <View style={styles.icon}>
-          {phone}
-        </View>
-        <View style={styles.icon}>
-          {camera}
-        </View>
-      </View>
+      <Swiper 
+        style={styles.wrapper} 
+        showsButtons={false} 
+        loop={false} 
+        showsPagination={false}
+      >
         <View>
-          <Text style={{color:'#fff'}}>Hello Swiper</Text>
+          <View style={styles.containerClock}>
+            <Text style={styles.hour}>{hour}</Text>
+            <Text style={styles.date}>{today}</Text>
+          </View>
+          <View style={styles.footer}>
+            <View style={styles.icon}>
+              {phone}
+            </View>
+            <View style={styles.icon}>
+              {camera}
+            </View>
+          </View>
+        </View>
+        <View style={{display: 'flex',flexGrow: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{color:colors.dark}}>This is where the menu should be</Text>
         </View>
       </Swiper>
-      {/* <Text style={{color:'#FFF'}}>Open up App.js to start working on your app!</Text> */}
       {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   app: {
     position: 'relative',
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: colors.main,
     alignItems: 'center',
     justifyContent: 'space-around',
   },
   containerClock: {
-    position: 'absolute',
+    alignSelf: 'center',
     top: 80,
+    margin: 'auto',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: "space-evenly" ,
-    borderColor: '#FFF',
-    borderWidth: 2,
+    borderColor: colors.dark,
+    borderWidth: 4,
     width: 200,
     height: 200,
-    borderRadius: 200,
+    borderRadius: 200
   },
   hour: {
     textAlign: 'center',
     textAlignVertical: 'center',
-    color: "#FFF",
+    color: colors.dark,
     fontSize: 40,
   },
   date: {
     textAlign: 'center',
     textAlignVertical: 'center',
-    color: "#FFF",
+    color: colors.dark,
     fontSize: 15,
     marginTop: -50
   },
   footer: {
     display: 'flex',
     flexDirection: 'row',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    borderWidth: 1,
+    borderColor: '#0f0'
   },
   icon: {
     padding: 30

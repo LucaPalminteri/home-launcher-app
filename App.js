f1646cb2678bad17af349b9d07a7223524497d9e
LucaPalminteri <lucapalminteri02@gmail.com>
2022-08-25 17:35:59 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#FFF" />;
 
 export default function App() {
+
+  const [hour,setHour] = useState('')
+  const [today,setToday] = useState('')
+  
+
+  useEffect(()=> {
+    let actualTime = new Date()
+    setToday(actualTime.toDateString());
+    setHour(actualTime.toLocaleTimeString().slice(0,5))
+  },[])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.app}>
+      <View style={styles.containerClock}>
+        <Text style={styles.hour}>{hour}</Text>
+      </View>
+      <View>
+          {myIcon}
+      </View>
+      {/* <Text style={{color:'#FFF'}}>Open up App.js to start working on your app!</Text> */}
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerClock: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: "space-evenly" ,
+    borderColor: '#FFF',
+    borderWidth: 2,
+    width: 200,
+    height: 200,
+    borderRadius: 200
+  },
+  hour: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: "#FFF",
+    fontSize: 40,
+  },
+  date: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: "#FFF",
+    fontSize: 15,
+    marginTop: -50
+  }
 });
